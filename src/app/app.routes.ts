a6fef97a594e941230887174b75f716dda0547f4
Clément Begnaud <clement.begnaud@gmail.com>
2024-11-22 09:23:41 -0500

    feat: configure routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { starshipsResolver } from './modules/starship/resolvers/starship.resolver';
+import { StarshipsPageComponent } from './modules/starship/components/starships-page/starships-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/starships',
+    pathMatch: 'full',
+  },
+  {
+    path: 'starships',
+    component: StarshipsPageComponent,
+    resolve: { starshipsData: starshipsResolver },
+  },
+];
