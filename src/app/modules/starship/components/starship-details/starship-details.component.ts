2bf20f0577460a93e1cf6af2e1e6e9337620a79b
Clément Begnaud <clement.begnaud@gmail.com>
2024-11-22 09:37:12 -0500

    feat: show and link pilots on starship details

--- src/app/modules/starship/components/starship-details/starship-details.component.ts
+++ src/app/modules/starship/components/starship-details/starship-details.component.ts
@@ -14,9 +14,17 @@ export class StarshipDetailsComponent {
   private peopleDialogService = inject(PeopleDialogService);
 
   @Input()
-  public set data(data: { selectedStarship?: Starship}) {
+  public set data(data: { selectedStarship?: Starship; people: People[] }) {
     this.starship = data.selectedStarship;
+    this.pilots = data.people.filter((peep) =>
+      this.starship?.pilots.includes(peep.url)
+    );
   }
 
   public starship?: Starship;
+  public pilots: People[] = [];
+
+  showPilot(pilot: People) {
+    this.peopleDialogService.showPeople(pilot);
+  }
 }
